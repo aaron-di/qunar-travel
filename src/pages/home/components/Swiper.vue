e259e6a2eed193bf9b13f619da55310987a9538c
Aaron Di <aarondi1119@gmail.com>
2022-07-21 21:58:36 +0800

    ✨ feat(Home): HomeSwiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1812/cb/cb4f4641fc2c5b4fa3.img.jpg_600x330_c7a53399.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1707/82/821428e3bbf93bbaa3.water.jpg_600x330_2385695d.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 55%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
