fbd161391dcb80291fc2efb5bd326254470ee5ce
Aaron Di <aarondi1119@gmail.com>
2022-07-23 00:23:11 +0800

    ✨ feat(Home): axios config

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -8,12 +8,13 @@
   </div>
 </template>
 
-<script>
+<script>   
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
 import HomeRecommend from './components/Recommend.vue'
 import HomeWeekend from './components/Weekend.vue'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
@@ -22,6 +23,18 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) { 
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
   }
 }
 </script>
